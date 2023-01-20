9fd4ce8b4c6ba88c7c4d9675ec3748c0eb230ee9
he-jaramillo <HE.JARAMILLO@duocuc.cl>
2023-01-20 00:59:11 -0300

    Initial commit

---
+++ src/main/java/cl/duoc/examenwebpayamb/ExamenWebpayAmbApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.examenwebpayamb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenWebpayAmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenWebpayAmbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/examenwebpayamb/ExamenWebpayAmbApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.examenwebpayamb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenWebpayAmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
