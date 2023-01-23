48f0a55fdb1a18b8475c6fd4c70245bbafb02e0a
MatiasBcEz <101440051+MatiasBcEz@users.noreply.github.com>
2023-01-23 18:47:50 -0300

    aa

---
+++ src/main/java/cl/duoc/examenwebpayamb/clients/WebPayFeignClient.java
@@ -0,0 +1,21 @@
+package cl.duoc.examenwebpayamb.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import cl.duoc.examenwebpayamb.model.dto.WebPayTransactionInitRequest;
+import cl.duoc.examenwebpayamb.model.dto.WebPayTransactionInitResponse;
+
+@FeignClient(name = "svc-webpay", url = "https://webpay3gint.transbank.cl")
+public interface WebPayFeignClient {
+    
+
+    @PostMapping(path = "/rswebpaytransaction/api/webpay/v1.2/transactions",  produces = { "application/json" })
+    public WebPayTransactionInitResponse initTransaction(
+        @RequestHeader("Tbk-Api-Key-Id") String apiKeyId, 
+        @RequestHeader("Tbk-Api-Key-Secret") String apiKeySecret,
+        @RequestBody WebPayTransactionInitRequest request
+        );
+}
